82a06c67fc378874dc3c42e2c012b47f0d08ceeb
glaucotodesco <glauco.todesco@gmail.com>
2024-09-06 14:07:59 -0300

    first commit

---
+++ src/main/java/com/aluno/aluno_backend/AlunoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aluno.aluno_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluno/aluno_backend/AlunoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluno.aluno_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
