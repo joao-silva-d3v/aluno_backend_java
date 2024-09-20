96d226e62d0c2f9682bbd32d99689cafa8223cac
glaucotodesco <glauco.todesco@gmail.com>
2024-09-20 15:09:19 -0300

    Seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO ALUNO(NAME, ACTIVE, PERIOD) VALUES ('Glauco Todesco', true,'Tarde');
+INSERT INTO ALUNO(NAME, ACTIVE, PERIOD) VALUES ('Marcia Silva', false,'Tarde');
+INSERT INTO ALUNO(NAME, ACTIVE, PERIOD) VALUES ('Antonio Ferraz', true,'Noite');
