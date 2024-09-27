27a56a2cc5a22f33ead165a4f52be74c9e7da7b7
glaucotodesco <glauco.todesco@gmail.com>
2024-09-27 15:00:43 -0300

    put, delete, post e get/id

--- src/main/java/com/aluno/aluno_backend/resources/AlunoController.java
+++ src/main/java/com/aluno/aluno_backend/resources/AlunoController.java
@@ -1,7 +1,12 @@
 package com.aluno.aluno_backend.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,4 +25,27 @@ public class AlunoController {
          return service.getAlunos();
       }
     
+      @GetMapping("students/{id}")
+      public Aluno getAlunoById(@PathVariable int id){
+         return service.getAlunoById(id);
+      }
+   
+      @PostMapping("students")
+      public Aluno save(@RequestBody Aluno aluno){
+            return service.save(aluno);
+      }
+
+      @DeleteMapping("students/{id}")
+      public void deleteById(@PathVariable int id){
+          service.deleteById(id);
+      }
+
+      @PutMapping("students/{id}")
+      public void update(@PathVariable int id,
+                         @RequestBody Aluno aluno){
+
+          service.update(id, aluno);
+          
+      }
+
 }
--- src/main/java/com/aluno/aluno_backend/services/AlunoService.java
+++ src/main/java/com/aluno/aluno_backend/services/AlunoService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.aluno.aluno_backend.entities.Aluno;
 import com.aluno.aluno_backend.repositories.AlunoRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 
 @Service
@@ -16,6 +18,33 @@ public class AlunoService {
 
     public List<Aluno> getAlunos(){
         return repository.findAll();
+    }
+
+    public Aluno getAlunoById(int id){
+        return repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Aluno não cadastrado!")
+        );
+    }
+
+    public Aluno save(Aluno aluno){
+        return repository.save(aluno);
+    }
+
+    public void deleteById(int id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("Aluno não cadastrado");
+        }
+    }
+
+    public void update(int id, Aluno aluno){
+        Aluno aux = repository.getReferenceById(id);
+        aux.setName(aluno.getName());
+        aux.setPeriod(aluno.getPeriod());
+        aux.setActive(aluno.getActive());
+        repository.save(aluno);
     }
     
 }
